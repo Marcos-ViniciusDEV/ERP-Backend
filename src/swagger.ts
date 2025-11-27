1c54e9ab44c63f8dd09ceafee3d5258286551b9f
Marcos Vinicius <marcosmarcosviniciusdigital@gmail.com>
2025-11-27 14:09:42 -0300

    fix: resolve __dirname issue in ES modules
    - Add fileURLToPath import from 'url'
    - Create __dirname equivalent using import.meta.url
    - Fix ReferenceError when loading openapi.yaml
    - Now compatible with ES module scope

--- src/swagger.ts
+++ src/swagger.ts
@@ -1,6 +1,11 @@
 import * as fs from 'fs';
 import * as path from 'path';
 import * as yaml from 'js-yaml';
+import { fileURLToPath } from 'url';
+
+// Get __dirname equivalent in ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Load OpenAPI YAML file
 const yamlFilePath = path.join(__dirname, '../openapi.yaml');
@@ -8,4 +13,5 @@ const yamlContent = fs.readFileSync(yamlFilePath, 'utf8');
 
 // Parse YAML to JSON
 export const swaggerSpec = yaml.load(yamlContent) as object;
+
 
